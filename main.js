01c521d44cc1c31af4a6f88be82c068e865c905c
slielsea <samirasaidkamolova@gmail.com>
2024-03-11 10:30:33 +0500

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let Prompt1 = prompt('String kiriting')
+
+
+let Prompt2 = prompt('Number kiriting')
+
+
+let Prompt3 = prompt('Boolean kiriting')
+alert(`Siz kiritgan soz ${Prompt2} rostanham data type number `);
+alert(`Siz kiritgan soz ${Prompt1} rostanham data type string`);
+alert(`Siz kiritgan soz ${Prompt3} rostanham data type boolean`);
